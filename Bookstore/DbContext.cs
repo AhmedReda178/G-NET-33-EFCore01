e10435c782e6f23da46edbf6c91d916b1b6d780b
AhmedReda178 <rda49586@gmail.com>
2026-04-23 22:22:52 +0200

    Answer 4: DbContext

---
+++ Bookstore/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+namespace Bookstore
+{
+    #region Answer 4: DbContext
+    public class BookStoreContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=.;Database=BookStoreDB;Trusted_Connection=True;");
+        }
+    } 
+    #endregion
+}
