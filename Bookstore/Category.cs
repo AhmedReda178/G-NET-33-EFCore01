ad05f95a0e00b083959a51efd3ef6c8d429aaa4b
AhmedReda178 <rda49586@gmail.com>
2026-04-23 22:20:10 +0200

    Answer 3: Category Entity

---
+++ Bookstore/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookstore
+{
+    #region Answer 3: Category Entity
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+
+        public List<Book> Books { get; set; }
+    } 
+    #endregion
+}
